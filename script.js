5cb11ec31e6e3d88183e040834c774fcf47f93bf
sterling#elysian.JP60=={56} <shohan.sub.56@gmail.com>
2022-07-08 14:28:47 +0600

    animation fr added

--- script.js
+++ script.js
@@ -6,6 +6,22 @@ const worldEnv = document.querySelector("[data-world]");
 setPixToWorldScale();
 window.addEventListener("resize", setPixToWorldScale);
 
+let lastTime;
+function update(time) {
+  if (lastTime == null) {
+    lastTime = time;
+    window.requestAnimationFrame(update);
+    return;
+  }
+
+  const delta = time - lastTime;
+  console.log(delta);
+
+  lastTime = time;
+  window.requestAnimationFrame(update);
+}
+window.requestAnimationFrame(update);
+
 function setPixToWorldScale() {
   let worldToPixScale;
   if (window.innerWidth / window.innerHeight < WORLD_WIDTH / WORLD_HEIGHT) {
